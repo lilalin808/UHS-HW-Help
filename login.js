23a389b5bfec694f2ef022c58313133bf8a000c6
lilalin808 <lilalanlin@icloud.com>
2025-03-08 21:18:36 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBmS3PF33c4BHzgjKuM0LUSu_wpIFQSNvk",
-  authDomain: "peer-tutor-a1076.firebaseapp.com",
-  projectId: "peer-tutor-a1076",
-  storageBucket: "peer-tutor-a1076.firebasestorage.app",
-  messagingSenderId: "677806357185",
-  appId: "1:677806357185:web:be5149be7ba68343517240",
-};import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -97,80 +86,6 @@ signIn.addEventListener("click", (event) => {
         showMessage("Incorrect Email or Password", "signInMessage");
       } else {
         showMessage("Account does not Exist", "signInMessage");
-      }
-    });
-});
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-function showMessage(message, divId) {
-  const messageDiv = document.getElementById(divId);
-  messageDiv.style.display = "block";
-  messageDiv.innerHTML = message;
-  messageDiv.style.opacity = 1;
-  setTimeout(() => messageDiv.style.opacity = 0, 5000);
-}
-
-const signUp = document.getElementById("submitSignUp");
-signUp.addEventListener("click", (event) => {
-  event.preventDefault();
-  const email = document.getElementById("rEmail").value;
-  const password = document.getElementById("rPassword").value;
-
-  const auth = getAuth();
-  const db = getFirestore();
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const userData = { email: email, role: "user" };  // Add more user fields here
-
-      showMessage("Account Created Successfully", "signUpMessage");
-
-      const docRef = doc(db, "users", user.uid);
-      setDoc(docRef, userData)
-        .then(() => {
-          window.location.href = "homepage.html";  // Redirect to login page
-        })
-        .catch((error) => {
-          console.error("Error writing document", error);
-        });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode == "auth/email-already-in-use") {
-        showMessage("Email Address Already Exists !!!", "signUpMessage");
-      } else {
-        showMessage("Unable to create User", "signUpMessage");
-      }
-    });
-});
-
-const signIn = document.getElementById("submitSignIn");
-signIn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  const auth = getAuth();
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      showMessage("Login successful", "signInMessage");
-      const user = userCredential.user;
-      localStorage.setItem("loggedInUserId", user.uid); // Store UID
-      window.location.href = "homepage.html";  // Redirect to homepage
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === "auth/wrong-password") {
-        showMessage("Incorrect Email or Password", "signInMessage");
-      } else if (errorCode === "auth/user-not-found") {
-        showMessage("Account does not Exist", "signInMessage");
-      } else {
-        showMessage("An error occurred. Please try again.", "signInMessage");
       }
     });
 });
